279ac7bcf858f0a889e2ef19c998c1866ed0b2ad
je <je@JM>
2018-09-01 01:53:58 +0800

    save

--- TestOpenGL/Source.cpp
+++ TestOpenGL/Source.cpp
@@ -4,6 +4,22 @@
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	cout << "reset window size" << endl;
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void renderer() {
+
+}
+
 int main() {
 
 	glfwInit();
@@ -29,9 +45,91 @@ int main() {
 
 	glViewport(0, 0, 800, 600);
 
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	int a;
-	cin >> a;
+
+	// shader vertex
+	const char *vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	const char *fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//glShaderSource(vertexShader, 1, vertexShaderSource, NULL);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infolog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
+		cout << "error::shader::vertex:compilation_failed" << endl;
+	}
+
+	// shader fragment
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	// 附加
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// 检查错误
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infolog);
+		cout << "error::shader::program:compilation_failed" << endl;
+	}
+
+	glUseProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		//renderer
+		//...
+		renderer();
+
+		float vertices[] = {
+			-0.5,-0.5,0,
+			0.5,0.5,0,
+			0,0.5,0
+		};
+
+		unsigned int VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glClearColor(0.3, 0.3, 0.5, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
