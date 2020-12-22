ee7cfd74a115304a5a19880162abab72756335a8
je <je@jm.com>
2020-12-23 01:52:24 +0800

    编译命令改为相对路径

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 @REM 编译命令
 
-g++.exe -g D:\root\TestOpenGL\TestOpenGL\Source.cpp -I D:\root\TestOpenGL\Ref\Include -LRef\Libs -lglfw3dll -o build\app
+g++.exe -g TestOpenGL\Source.cpp -I Ref\Include -LRef\Libs -lglfw3dll -o build\app
