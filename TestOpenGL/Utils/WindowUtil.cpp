97397b5712d4650b70a476dd0002a71886e0e197
je <je@jueane.com>
2025-08-15 17:48:20 +0800

    窗口位置优化

--- TestOpenGL/Utils/WindowUtil.cpp
+++ TestOpenGL/Utils/WindowUtil.cpp
@@ -7,13 +7,13 @@
 #include "../Feature/CameraTemp.h"
 #include <glad/glad.h>
 
-
 using namespace std;
 
 int WindowUtil::width = 0;
 int WindowUtil::height = 0;
 
-GLFWwindow *WindowUtil::createWindowUtil(int width, int height) {
+GLFWwindow *WindowUtil::createWindowUtil(int width, int height)
+{
 
     WindowUtil::width = width;
     WindowUtil::height = height;
@@ -24,8 +24,26 @@ GLFWwindow *WindowUtil::createWindowUtil(int width, int height) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    auto console = GetConsoleWindow();
+    RECT rect;
+    GetWindowRect(console, &rect);
+    auto consoleX = rect.left;
+    auto consoleY = rect.top;
+    cout << "控制台窗口坐标：" << consoleX << ", " << consoleY << endl;
+    if (consoleX == 0 && consoleY == 0)
+    {
+        consoleX = 900;
+        consoleY = 300;
+    }
+    else
+    {
+        consoleX += 800;
+        // consoleY = 600;
+    }
+
     GLFWwindow *window = glfwCreateWindow(width, height, "My Game", NULL, NULL);
-    if (window == nullptr) {
+    if (window == nullptr)
+    {
         cout << "create gl window failed" << endl;
         glfwTerminate();
         return nullptr;
@@ -33,6 +51,7 @@ GLFWwindow *WindowUtil::createWindowUtil(int width, int height) {
 
     cout << "Window created" << endl;
     glfwSetFramebufferSizeCallback(window, WindowUtil::framebuffer_size_callback);
+    glfwSetWindowPos(window, consoleX, consoleY);
     glfwMakeContextCurrent(window);
 
     glfwSetCursorPosCallback(window, CameraTemp::mouse_callback);
@@ -40,7 +59,8 @@ GLFWwindow *WindowUtil::createWindowUtil(int width, int height) {
 
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
         cout << "failed init glad" << endl;
         return nullptr;
     }
@@ -48,7 +68,8 @@ GLFWwindow *WindowUtil::createWindowUtil(int width, int height) {
     return window;
 }
 
-void WindowUtil::framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+void WindowUtil::framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
     cout << "reset window size" << endl;
     glViewport(0, 0, width, height);
 }
